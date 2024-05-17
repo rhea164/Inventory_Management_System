00bea1fa55fb511bad82baf5873e27ecbdf72922
rhea164 <158840696+rhea164@users.noreply.github.com>
2024-05-17 12:13:08 +0400

    Initial commit

---
+++ uimodule/webapp/controller/Products.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+    "tutorial/products/controller/BaseController"
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("tutorial.products.controller.Products", {
+
+        onInit: function () {
+            this.addHistoryEntry({
+                title: "All Products",
+                icon: "sap-icon://product",
+                intent: "#display-uimodule"
+            }, true);
+        },
+
+
+        handleListItemPress: function (oEvent) {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            var selectedProductId = oEvent.getSource().getBindingContext().getProperty("ProductID");
+            oRouter.navTo("ProductDetail", {
+                productId: selectedProductId
+            });
+        }
+
+    });
+});
